24aa2f88bde1a0c3fced7dcfa6dd1e35b0968902
luoxyyf <645427466@qq.com>
2024-09-01 16:06:43 +0800

    chore: config

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FormEditor,
   Material,
@@ -6,23 +7,29 @@ import {
 } from "@roddan/form-editor";
 
 export default function EditorDesgin() {
+  const [loading, setLoading] = useState(false);
   return (
     <FormEditor
       mode="design"
       actionProp={{
         previewUrl: "/editor-page/form",
         download() {
+          if (loading) return;
+          setLoading(true);
+          console.log("download");
           try {
-            const link = document.createElement('a');
-            link.href = 'https://roddan.cn/editor-assets/page.zip';
-            link.download = 'page.zip'; // 你可以指定下载的文件名
+            const link = document.createElement("a");
+            link.href = "https://roddan.cn/editor-assets/page.zip";
+            link.download = "page.zip"; // 你可以指定下载的文件名
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
           } catch (error) {
-            console.error('Error downloading the file:', error);
+            console.error("Error downloading the file:", error);
+          } finally {
+            setLoading(false);
           }
-        }
+        },
       }}
     >
       <Material />
